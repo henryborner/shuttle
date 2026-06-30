b993afb2b94bed88874a87d6d7eec6a552e3e555
henryborner <2871819549@qq.com>
2026-06-30 19:03:03 +0800

    version: 0.1.4.8 → 0.1.5.0

--- internal/i18n/en.go
+++ internal/i18n/en.go
@@ -7,7 +7,7 @@ func init() {
 		// App
 		"app.title":        "Shuttle",
 		"app.subtitle":     "Incremental File Sync",
-		"app.version":      " v0.1.4.8",
+		"app.version":      " v0.1.5.0",
 		"app.connected":    "Connected",
 		"app.disconnected": "Disconnected",
 
