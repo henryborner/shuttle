025216d702412462dcbc63a470dd6d5a8de9a0e2
henryborner <2871819549@qq.com>
2026-07-21 19:49:24 +0800

    version: bump to 0.1.5.4

--- cmd/shuttle/main.go
+++ cmd/shuttle/main.go
@@ -24,7 +24,7 @@ var (
 	algoName   string
 	schemaFlag bool
 
-	versionStr = "0.1.5.3"
+	versionStr = "0.1.5.4"
 	rootCmd    = &cobra.Command{
 		Use:   "shuttle",
 		Short: "Incremental file sync over SSH",
