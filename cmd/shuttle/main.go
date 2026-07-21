643f24e65cf814290f54b07fbe788a3edc4026db
henryborner <2871819549@qq.com>
2026-07-21 20:28:32 +0800

    version: bump to 0.1.5.6

--- cmd/shuttle/main.go
+++ cmd/shuttle/main.go
@@ -24,7 +24,7 @@ var (
 	algoName   string
 	schemaFlag bool
 
-	versionStr = "0.1.5.5"
+	versionStr = "0.1.5.6"
 	rootCmd    = &cobra.Command{
 		Use:   "shuttle",
 		Short: "Incremental file sync over SSH",
--- internal/i18n/en.go
+++ internal/i18n/en.go
@@ -7,7 +7,7 @@ func init() {
 		// App
 		"app.title":        "Shuttle",
 		"app.subtitle":     "Incremental File Sync",
-		"app.version":      " v0.1.5.5",
+		"app.version":      " v0.1.5.6",
 		"app.connected":    "Connected",
 		"app.disconnected": "Disconnected",
 
